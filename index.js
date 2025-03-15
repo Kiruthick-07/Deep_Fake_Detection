6f5c709e62d8af6207aec03a5dda5e8b4004c0a0
Kiruthick-07 <tamilelectroniv@gmail.com>
2025-03-15 11:52:45 +0530

    mobile view on progress

--- index.js
+++ index.js
@@ -28,3 +28,11 @@ submitBtn.addEventListener('click', (e) => {
         reader.readAsDataURL(file);
     }
 });
+function toggleMenu() {
+    var mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
